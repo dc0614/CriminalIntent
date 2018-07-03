a32adbcea5e601fd206b451a04af2286aeee5166
D Copeland <dcope0614@gmail.com>
2018-07-02 23:33:38 -0400

    This is the challenge for chapter (Criminal Intent)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,6 +2,11 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
+/*
+ @author: Diana Copeland
+ Challenge for Chapter 8
+ */
+
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
