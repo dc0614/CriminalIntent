774570b43d1b3ee1fcc8c6b29715a1695d6abf68
D Copeland <dcope0614@gmail.com>
2018-07-02 23:28:50 -0400

    This is the initial commit for project Criminal Intent (Chapter 7 & 8) of The Big Nerd Ranch: Android Programming Third Edition.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
